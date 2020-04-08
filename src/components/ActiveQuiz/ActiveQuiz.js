3fad329815cb6eeca90aa012544ff43826b9ffd7
Igor Korenets <vik753@gmail.com>
2020-04-08 18:16:13 +0300

    created answerList and answerItem and some test data into Quiz

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import classes from './ActiveQuiz.module.css'
+import AnswersList from './AnswersList/AnswersList';
+
 
 const ActiveQuiz = (props) => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <strong>2.</strong>&nbsp;
-        How do you do?
+        <strong>2.</strong>&nbsp; How do you do?
       </span>
       <small>4 from 12</small>
     </p>
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
+      <AnswersList answers={props.answers} />
     </ul>
   </div>
 );
